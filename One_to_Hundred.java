25cac428a37bf518c3b1e7663d7080eaca195c73
Vishnuu6 <vishnubanapuram.04@gmail.com>
2024-12-11 19:48:01 +0530

    updated format

--- One_to_Hundred.java
+++ One_to_Hundred.java
@@ -1,6 +1,6 @@
 public class One_to_Hundred {
     public static void main(String[] args) {
-        System.out.println("Prime numbers between 1 and 100:");
+        System.out.println("Prime numbers betwee 1 and 100:");
         for (int num = 2; num <= 100; num++) {
             boolean isPrime = true;
             for (int i = 2; i <= num / 2; i++) {
